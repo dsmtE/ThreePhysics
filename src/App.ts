38e1158c0b9194d346de1fef8fa0ee948fc3d55a
Enguerrand DE SMET <desmet.enguerrand@gmail.com>
2021-03-23 13:32:55 +0100

    add clean physics update and scene mamangment

--- src/App.ts
+++ src/App.ts
@@ -79,11 +79,10 @@ export default class App {
         // add Events Global
         window.addEventListener('resize', this.onWindowResize.bind(this), false);
 
-        this.setupPhysics();
-        this.simulation(0); // used to display and update objects properly once setup done
+        this.dots = [];
+        this.constraints = [];
 
-        for (let d of this.dots) d.addToscene(this.scene);
-        for (let c of this.constraints) c.addToscene(this.scene);
+        this.setupPhysics();
 
         this.lastTimeDisplay = Date.now();
         this.lastTimeUpdate = Date.now();
@@ -94,23 +93,34 @@ export default class App {
     }
 
     setupPhysics() {
-
+        // clean if needed
+        this.dots.forEach(d => d.removeFromScene());
+        this.constraints.forEach(c => c.removeFromScene());
+        
         this.dots = [];
         this.constraints = [];
         this.param.windDir = new Vector3(1, 0, 0);
         this.param.windStrength = 10;
         
+        const m: number = 1;
+        const h: number = this.param.physicUpdateFps;
+
         const size: number = 10;
         for (let i = 0; i < size; ++i)
-            this.dots.push(new Dot(1, new THREE.Vector3((-size/2+i)*5, 1, 0), 0x0000ff, (i == 0 || i == size-1) ? Dot.Type.Fix : Dot.Type.notFix));
+            this.dots.push(new Dot(m, new THREE.Vector3((-size/2+i)*5, 1, 0), 0x0000ff, (i == 0 || i == size-1) ? Dot.Type.Fix : Dot.Type.notFix));
 
         for (let i = 0; i < size-1; ++i)
-            this.constraints.push(new Spring(this.dots[i], this.dots[i+1], 50));
+            this.constraints.push(new Spring(this.dots[i], this.dots[i+1], 1 * m * h*h ));
 
         // initial conditions
         this.dots[Math.ceil(size/2)].pos.y -= 2;
         // for (let i = 0; i < size; ++i)
         //     this.dots[i].pos.y -= (size-Math.abs(size/2-i));
+
+        for (let d of this.dots) d.addToscene(this.scene);
+        for (let c of this.constraints) c.addToscene(this.scene);
+
+        this.simulation(0); // used to display and update objects properly once setup done
     }
 
     setupScene() {
@@ -190,7 +200,7 @@ export default class App {
         optionFolder.open();
 
         const PhysicFolder = this.gui.addFolder('physic options');
-        PhysicFolder.add(this.param, 'physicUpdateFps', 1, 250, 1);
+        PhysicFolder.add(this.param, 'physicUpdateFps', 1, 250, 1).onChange(() => this.setupPhysics());
         PhysicFolder.add(this.param, 'integrationType', ["Verlet", "EulerExp"]);
         const wFolder = this.gui.addFolder('windDir');
         wFolder.add(this.param.windDir, 'x');
--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -6,13 +6,29 @@ export abstract class Drawable {
     protected mat?: THREE.Material;
     protected model?: THREE.Object3D;
 
+    private scene: THREE.Scene;
+
     constructor() {}
 
     abstract updateDraw(): void;
     abstract initDrawable(): void;
 
     addToscene(scene: THREE.Scene): void {
-        scene.add(this.model);
+        if(this.scene == null) {
+            this.scene = scene;
+            this.scene.add(this.model);
+        }else {
+            console.log('already in one scene');
+        }
+    }
+
+    removeFromScene(): void {
+        if(this.scene != null) {
+            this.scene.remove(this.model);
+            this.scene = null;
+        }else {
+            console.log('not in any scene currently');
+        }
     }
 }
 
